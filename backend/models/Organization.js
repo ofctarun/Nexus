0fc639f6bb9d219ac286e52bdbf13ca121da42d4
Umeshh27 <23MH1A05E7@acoe.edu.in>
2026-05-06 21:35:46 +0530

    try1

--- backend/models/Organization.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const organizationSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  inviteCode: { type: String, required: true, unique: true },
-  admin: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
-}, { timestamps: true });
-
-export default mongoose.model('Organization', organizationSchema);
